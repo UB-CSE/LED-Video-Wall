e2266bb50a718b3cb4e0ce931dc9531d038ae466
Alex <asbohosi@buffalo.edu>
2025-04-03 12:07:16 -0400

    Added ns-per-tick and ns-per-frame values to config file.

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -2,6 +2,7 @@
 #include <exception>
 #include <iostream>
 #include <iterator>
+#include <optional>
 #include <string.h>
 #include <netinet/in.h>
 #include <string>
@@ -26,16 +27,17 @@ int main(int argc, char* argv[]) {
         exit(-1);
     }
 
-    std::pair<std::vector<Client*>, cv::Size> clients_exp;
+    std::optional<ServerConfig> server_config_opt;
     try {
-        clients_exp = parse_config_throws("config.yaml");
+        server_config_opt = parse_config_throws("config.yaml");
     } catch (std::exception& ex) {
         std::cerr << "Error Parsing config file: " << ex.what() << "\n";
         exit(-1);
     }
+    ServerConfig server_config = server_config_opt.value();
 
-    std::cout << "Size: " << clients_exp.second << "\n";
-    VirtualCanvas vCanvas(clients_exp.second);
+    std::cout << "Size: " << server_config.canvas_size << "\n";
+    VirtualCanvas vCanvas(server_config.canvas_size);
 
     std::map <std::string, std::vector<std::vector<Element>>> elements;
     try {
@@ -48,22 +50,23 @@ int main(int argc, char* argv[]) {
     }
     vCanvas.addPayloadToCanvas(elements);
 
-    for (Client* c : clients_exp.first) {
+    for (Client* c : server_config.clients) {
         std::cout << c->to_string() << "\n";
     }
 
     std::optional<LEDTCPServer> server_opt =
-        create_server(INADDR_ANY, 7070, 7074, clients_exp.first);
+        create_server(INADDR_ANY, 7070, 7074, server_config.clients);
     if (!server_opt.has_value()) {
         exit(-1);
     }
     LEDTCPServer server = server_opt.value();
     server.start();
 
-    uint64 ns_per_tick = 010'000'000; // 100 tps
-    uint64 ns_per_frame = 200'000'000; // 5 fps
-
-    Controller cont(vCanvas, clients_exp.first, server, ns_per_tick, ns_per_frame);
+    Controller cont(vCanvas,
+                    server_config.clients,
+                    server,
+                    server_config.ns_per_tick,
+                    server_config.ns_per_frame);
 
     while(1) {
         cont.tick_exec();
