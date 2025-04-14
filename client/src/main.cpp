e674d0a66f3fd4ac9d477b821ed43e83efc28f2e
ok-nick <nick.libraries@gmail.com>
2025-04-13 21:28:02 -0400

    use app_main instead of Arduino setup and loop

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -7,23 +7,25 @@
 
 static const char *TAG = "Main";
 
-void setup() {
+extern "C" void app_main(void) {
+  initArduino();
+
   Serial.begin(115200);
   connect_wifi();
-}
 
-void loop() {
-  if (!socket.connected()) {
-    ESP_LOGI(TAG, "Reconnecting to server...");
-    send_checkin();
-    vTaskDelay(pdMS_TO_TICKS(CHECK_IN_DELAY_MS));
-    return;
+  while (true) {
+    if (!socket.connected()) {
+      ESP_LOGI(TAG, "Reconnecting to server...");
+      send_checkin();
+      vTaskDelay(pdMS_TO_TICKS(CHECK_IN_DELAY_MS));
+      return;
+    }
+
+    if (socket.available()) {
+      parse_tcp_message();
+    }
+
+    // To prevent the task watchdog timer.
+    vTaskDelay(pdMS_TO_TICKS(10));
   }
-
-  if (socket.available()) {
-    parse_tcp_message();
-  }
-
-  // To prevent the task watchdog timer.
-  vTaskDelay(pdMS_TO_TICKS(10));
 }
