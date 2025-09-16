e29677a553e20e547cd9ec9341fbca0256da3cdb
Torin Sheahen <torinsheahen@gmail.com>
2025-09-16 12:32:01 -0400

    Just changed the picture

--- web/LED-Wall-Website/src/App.tsx
+++ web/LED-Wall-Website/src/App.tsx
@@ -5,7 +5,7 @@ function App(){
 
   return(
     <div className={styles.canvas}>
-      <Element url="https://imgs.search.brave.com/vzVb420qkaR-r4_3HMcbvH5eNniSvO1Ieq69YJzhYxY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pLmlt/Z2ZsaXAuY29tLzQv/M3UwNGg1LmpwZw" size={100}/>
+      <Element url="https://media.tenor.com/cb9L14uH-YAAAAAM/cool-fun.gif" size={100}/>
       <Element url="https://imgs.search.brave.com/uUrbr6g_HS4YuAgC3JWy5KIRWZnSrn3spfDMZJHmLGY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLmt5/bS1jZG4uY29tL3Bo/b3Rvcy9pbWFnZXMv/bGlzdC8wMDMvMDk5/LzMzNi84NWMuanBn" size={100}/>
     </div>
   );
