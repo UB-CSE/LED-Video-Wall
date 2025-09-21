f6122fbd719e0512296f6770799de4dfa91fca66
Torin Sheahen <torinsheahen@gmail.com>
2025-09-20 22:31:14 -0400

    Web client now gets images from the web server's static folder

--- web/LED-Wall-Website/src/App.tsx
+++ web/LED-Wall-Website/src/App.tsx
@@ -2,11 +2,14 @@ import styles from './Styles.module.css';
 import Element from './components/element';
 
 function App(){
+  function get_image(filename: String){
+    return 'http://127.0.0.1:5000/static/image/'+filename;
+  }
 
   return(
     <div className={styles.canvas}>
       <Element url="https://media.tenor.com/cb9L14uH-YAAAAAM/cool-fun.gif" size={100}/>
-      <Element url="https://imgs.search.brave.com/uUrbr6g_HS4YuAgC3JWy5KIRWZnSrn3spfDMZJHmLGY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLmt5/bS1jZG4uY29tL3Bo/b3Rvcy9pbWFnZXMv/bGlzdC8wMDMvMDk5/LzMzNi84NWMuanBn" size={100}/>
+      <Element url={get_image('img.jpg')} size={100}/>
     </div>
   );
 }
