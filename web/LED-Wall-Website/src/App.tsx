cdc30643c12a6c0c93651c266f1ea26cd94acc16
Torin Sheahen <torinsheahen@gmail.com>
2025-09-23 10:03:49 -0400

    App should now fetch a configuration, pass props to the elements and display any number of elements

--- web/LED-Wall-Website/src/App.tsx
+++ web/LED-Wall-Website/src/App.tsx
@@ -1,13 +1,35 @@
 import styles from './Styles.module.css';
 import Element from './components/element';
-import {useEffect} from 'react';
+import {useEffect, type JSX} from 'react';
 
 function App(){
   function get_image(filename: String){
-    return 'http://127.0.0.1:5000/static/image/'+filename;
+    return 'http://127.0.0.1:5000/static/'+filename;
   }
   
-  
+  const elements: JSX.Element[] = []
+
+  async function get_config(){
+    try{
+      const response = await fetch('/get_yaml_Config', {'method': 'GET'})
+      const config = await response.json()
+      for(const element of config['elements']){
+        elements.push(
+        <Element 
+          key={element['id']} 
+          type={element['type']} 
+          path={get_image(element['filepath'])} 
+          location={element['location']} 
+          size={100} 
+          />
+        );
+      }
+    } catch(error){
+      console.log('Get Config encountered an error: ' + error)
+      return
+    }
+  }
+
   const handleDrop = (e : DragEvent) => {
     e.preventDefault();
   };
@@ -21,6 +43,7 @@ function App(){
     document.addEventListener('drop', handleDrop);
     document.addEventListener('dragover', handleDrag);
     document.addEventListener('dragenter', handleDrag);
+    get_config()
     return () => {
       document.removeEventListener('drop', handleDrop);
       document.removeEventListener('dragover', handleDrag);
@@ -28,12 +51,9 @@ function App(){
     }
   },[]);
   
-  
-
   return(
     <div className={styles.canvas}>
-      <Element url="https://media.tenor.com/cb9L14uH-YAAAAAM/cool-fun.gif" size={100}/>
-      <Element url={get_image('img.jpg')} size={100}/>
+      {elements}
     </div>
   );
 }
