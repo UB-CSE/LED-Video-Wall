0299f530619ff87ee4f0058430ee6f0de008eee1
Jacob Bemis <jacobbemis04@gmail.com>
2025-09-22 23:41:57 -0400

    Made a Flask endpoint that will serve the LEDVW's server the configuration files, still a work in progress

--- web/web-server/main.py
+++ web/web-server/main.py
@@ -1,9 +1,12 @@
+import yaml
 
 from flask import Flask, request, jsonify
 #initialize an empty dictionary to store image coordinates, (x, y)
 imageCoords = {}
 
 app = Flask(__name__)
+
+config_File = "server/input-conway.yaml" #will change so that config_File could be any yaml file saved in the repo, not just input-conway
 
 @app.route('/get_Data', methods = ['GET', 'POST'])
 def get_Data():
@@ -23,6 +26,23 @@ def get_Data():
     print(f"Image Coordinates: ({x}, {y})")
     print("Image ID: "+ imgId)
     return "Main Communication"
+
+
+@app.route('/get_yaml_Config', methods = ['GET'])
+def get_yaml_Config():
+    try:
+        with open(config_File, "r") as file:
+            config_Data = yaml.safe_load(file)
+        
+        if ("settings" not in config_Data or "elements" not in config_Data):
+            print("[ERROR]: Invalid configuration format")
+            jsonify({"[ERROR]: Invalid configuration format"})
+        
+        return jsonify(config_Data) #Sends the client a JSON file that follows the YAML configuration 
+    except FileNotFoundError:
+        return jsonify({"[ERROR]: Configuration file, {config_file}, not found"}), 404
+
+
 
 if __name__ == "_main_":
     app.run(host="0.0.0.0")
