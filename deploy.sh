1e91399e0c02a795e2d0b4924b15f1c9e8d6cd51
Torin Sheahen <torinsheahen@gmail.com>
2025-10-22 00:58:24 -0400

    deploy.sh now restarts the server if already running or starts it otherwise

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,9 @@ npm --prefix ./web/LED-Wall-Website run build
 
 echo "Copying to distribution..."
 cp ./web/LED-Wall-Website/dist ../dist
+
+echo "Starting virtual environment..."
+source ../venv/bin/activate
+
+echo "Restarting server if already running or starting server otherwise..."
+pkill -HUP gunicorn || gunicorn --bind 127.0.0.1:5000 'web.web-server.main:app'
