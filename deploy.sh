e2cb5400a7867a571df72282ccbcec183cd5ef5c
Torin Sheahen <torinsheahen@gmail.com>
2025-10-22 11:10:51 -0400

    Bug fixing deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,10 @@ echo "Building new deployment..."
 npm --prefix ./web/LED-Wall-Website run build
 
 echo "Copying to distribution..."
-cp ./web/LED-Wall-Website/dist ../dist
+cp -r ./web/LED-Wall-Website/dist ../dist
 
 echo "Starting virtual environment..."
-source ../venv/bin/activate
+source ./venv/bin/activate
 
 echo "Restarting server if already running or starting server otherwise..."
 pkill -HUP gunicorn || gunicorn --bind 127.0.0.1:5000 'web.web-server.main:app'
