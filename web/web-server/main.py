2f2c6536f28d3924681d8e06c4001b6fabb39001
Torin Sheahen <torinsheahen@gmail.com>
2025-12-03 11:53:02 -0500

    The attempted fix had no effect, so I reverted it

--- web/web-server/main.py
+++ web/web-server/main.py
@@ -200,7 +200,7 @@ def start_server():
         )
         
         server_process = subprocess.Popen(
-            ["./led-wall-server", server_config_File, "--port", "7070"],
+            ["./led-wall-server", server_config_File],
             cwd=exe_dir,
             preexec_fn = os.setsid
         )
