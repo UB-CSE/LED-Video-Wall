6f64d5186e50a3d76a6b0689a017cf6fbcee248f
petelilley <hi.petelilley@gmail.com>
2026-04-24 22:35:36 -0400

    Server use different CEF cache directories and command pipe files depending on ledvw port. Also fixed apparent merge conflicts from earlier

--- server/inc/tcp.hpp
+++ server/inc/tcp.hpp
@@ -74,6 +74,7 @@ public:
 
 std::shared_ptr<LEDTCPServer> create_server(uint32_t addr,
                                             uint16_t port,
-                                            std::vector<Client*> clients);
+                                            std::vector<Client*> clients,
+                                            float brightness_percent);
 
 #endif
